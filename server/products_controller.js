7164a23b0255ac3dcd75dcbb3a7e6de6bddf9348
SecretMNC <pettkevinj@gmail.com>
2017-10-13 14:45:13 -0600

    added new endpoints

--- server/products_controller.js
+++ server/products_controller.js
@@ -1,5 +1,19 @@
 module.exports = {
     getAllBins: function (req, res){
-        req.app.get('db').get_shelf(req.params.id).then()
+        req.app.get('db').get_shelf(req.params.id)
+            .then( e => res.status(200).send(e) )
+            .catch( () => res.status(500).send() );
+    },
+
+    getOneBin: function (req, res){
+        req.app.get('db').get_bin(req.params.id)
+            .then (e => res.status(200).send(e) )
+            .catch( () => res.status(500).send() );
+    },
+
+    updateBin: function (req, res){
+        req.app.get('db').update_bin(req.params.id)
+            .then ()
     }
+
 };
