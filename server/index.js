95bcd31348e2e0f09b7a15f237ef46190536c86a
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-13 13:58:42 -0600

    beginings of the endpoints

--- server/index.js
+++ server/index.js
@@ -3,15 +3,26 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       massive = require('massive'),
       ctrl = require('./products_controller');
-      
+
       require('dotenv').config();
 
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 massive(process.env.CONNECTION_STRING).then(db =>{
+    console.log('massive works')
     app.set('db', db)});
 
+
+app.get('/api/shelf/:id')
+
+app.get('api/bin/:id')
+
+app.put('/api/bin/:id')
+
+app.delete('/api/bin/:id')
+
+api.post('/api/bin/:id')
 
 const port = process.env.PORT || 8080
 app.listen( port, () => console.log(`You are listening to a server on port ${port}!`))
