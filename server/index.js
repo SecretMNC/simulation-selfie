47b1e7afc0be0bf462aceefd56a4e9904db19cd8
SecretMNC <pettkevinj@gmail.com>
2017-10-13 12:11:14 -0600

    added a basic server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-test
+const express = require('express'),
+      cors = require('cors'),
+      bodyParser = require('body-parser'),
+      massive = require('massive'),
+      ctrl = require('./product_controller');
+      require('dotenv').config();
+
+const app = express();
+app.use( bodyParser.json() );
+app.use( cors() );
+massive(process.env.CONNECTION_STRING).then(db =>{
+    app.set('db', db)});
+
+
+const port = process.env.PORT || 8080
+app.listen( port, () => console.log(`You are listening to a server on port ${port}!`))
